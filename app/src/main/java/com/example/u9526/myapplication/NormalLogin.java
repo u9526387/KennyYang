07ba83ed9bc66f33fc8f3db802acc58f75e47d08
u9526387 <u9526387@gmail.com>
2018-06-19 18:32:14 +0800

    做完主頁的圖示跟文字，試試看merge

--- app/src/main/java/com/example/u9526/myapplication/NormalLogin.java
+++ app/src/main/java/com/example/u9526/myapplication/NormalLogin.java
@@ -4,19 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class NormalLogin extends AppCompatActivity {
 
+    private Button FinishRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal_login);
 
+        FinishRegister = (Button) findViewById(R.id.FinishRegister);
+        FinishRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent FinishRegi = new Intent(NormalLogin.this,homepage.class);
+                startActivity(FinishRegi);
+            }
+        });
 
     }
 
-    public void jumpTohomepage(View v){
-        Intent l=new Intent(this,homepage.class);
-        startActivity(l);
+
     }
-}
+
