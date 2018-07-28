be7a845a8786a89462a3ec8d8029cfd67409e558
xswzaq321 <zaq2344231@gmail.com>
2018-07-28 18:51:01 +0800

    jessy

--- app/src/main/java/com/example/u9526/myapplication/NormalLogin.java
+++ app/src/main/java/com/example/u9526/myapplication/NormalLogin.java
@@ -1,30 +1,66 @@
 package com.example.u9526.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class NormalLogin extends AppCompatActivity {
 
-    private Button FinishRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal_login);
 
-        FinishRegister = (Button) findViewById(R.id.FinishRegister);
-        FinishRegister.setOnClickListener(new View.OnClickListener() {
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button FinishLogin = (Button) findViewById(R.id.FinishLogin);
+
+        FinishLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent FinishRegi = new Intent(NormalLogin.this,homepage.class);
-                startActivity(FinishRegi);
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                                                    try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+
+                                if(success && username!= null && password != null){
+                                    Intent intent = new Intent(NormalLogin.this, homepage.class);
+                                    NormalLogin.this.startActivity(intent);
+                                }else{
+                                    AlertDialog.Builder builder  = new AlertDialog.Builder(NormalLogin.this);
+                                    builder.setMessage("Login Fail")
+                                            .setNegativeButton("Retry" ,null)
+                                            .create()
+                                            .show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                                    }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(username ,password , responseListener);
+                RequestQueue queue = Volley.newRequestQueue(NormalLogin.this);
+                queue.add(loginRequest);
             }
         });
-
     }
-
 
     }
 
--- app/src/main/java/com/example/u9526/myapplication/NormalRegister.java
+++ app/src/main/java/com/example/u9526/myapplication/NormalRegister.java
@@ -1,31 +1,78 @@
 package com.example.u9526.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class NormalRegister extends AppCompatActivity {
-
-    private Button FinishRegister;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal_register);
 
-        FinishRegister = (Button) findViewById(R.id.FinishRegister);
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText passworcheck = (EditText) findViewById(R.id.passwordcheck);
+        final Button FinishRegister = (Button) findViewById(R.id.FinishRegister);
+
         FinishRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(NormalRegister.this,homepage.class);
-                startActivity(i);
-                //i = 註冊完打開主頁;
+                final String name = etName.getText().toString();
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                final String passwordcheck = passworcheck.getText().toString();
+
+
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success && (password == passwordcheck)){//PROBLEM
+                                Intent intent = new Intent(NormalRegister.this,NormalLogin.class);
+                                NormalRegister.this.startActivity(intent);
+                            }else if(password != passwordcheck){
+                                AlertDialog.Builder builder  = new AlertDialog.Builder(NormalRegister.this);
+                                builder.setMessage("checkpassword wrong")
+                                        .setNegativeButton("Retry" ,null)
+                                        .create()
+                                        .show();
+                            }else{
+                                AlertDialog.Builder builder  = new AlertDialog.Builder(NormalRegister.this);
+                                builder.setMessage("Register Fail")
+                                        .setNegativeButton("Retry" ,null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(name, username, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(NormalRegister.this);
+                queue.add(registerRequest);
+
             }
         });
-
-
 
     }
 }
