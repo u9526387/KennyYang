6ec140f159f0d3764f5089343429157f604c1854
u9526387 <u9526387@gmail.com>
2018-09-06 02:19:42 +0800

    測試測試9/6

--- app/src/main/java/com/example/u9526/myapplication/FillRentInfo5.java
+++ app/src/main/java/com/example/u9526/myapplication/FillRentInfo5.java
@@ -1,21 +1,162 @@
 package com.example.u9526.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class FillRentInfo5 extends AppCompatActivity {
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.jar.Manifest;
+
+public class FillRentInfo5 extends AppCompatActivity implements View.OnClickListener{
 
 
+    private Button buttonChoose,buttonUpload;
+    private ImageView imageView;
+    private EditText editText;
+
+
+    private static final int STORAGE_PERMISSION_CODE = 2342;
+    private static final int PICK_IMAGE_REQUEST = 22;
+
+    private Uri filePath;
+    private Bitmap bitmap;
+
+    private static final String UPLOAD_URL = "http://10.0.2.2/UploadExample/upload.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_rent_info5);
 
+        buttonChoose = (Button) findViewById(R.id.buttonChoose);
+        buttonUpload = (Button) findViewById(R.id.buttonUpload);
+
+        imageView = (ImageView) findViewById(R.id.image);
+        editText = (EditText) findViewById(R.id.editText);
+
+        buttonChoose.setOnClickListener(this);
+        buttonUpload.setOnClickListener(this);
+
+        }
+
+
+    private void uploadMultipart(){
+        String name = editText.getText().toString();
+        String path = getPath(filePath);
+
+        try {
+            String uploadId = UUID.randomUUID().toString();
+            new MultipartUploadRequest(this, uploadId, UPLOAD_URL)
+                    .addFileToUpload(path, "image")
+                    .addParameter("name", name)
+                    .setNotificationConfig(new UploadNotificationConfig())
+                    .setMaxRetries(2)
+                    .startUpload();
+        }
+        catch(Exception e){
+
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        }
+
+
+    }
 
 
 
+    private void showFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "select picture"), PICK_IMAGE_REQUEST);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+             filePath = data.getData();
+             try{
+                 bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                 imageView.setImageBitmap(bitmap);
+             }
+             catch(IOException e){
+                 e.printStackTrace();
+             }
+        }
+    }
+
+    private String getPath(Uri uri){
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        cursor.close();
+
+        cursor = getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
+
+        }
+
+    private void requestStoragePermission(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted" , Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, "Permission not granted" , Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonUpload){
+            uploadMultipart();
+        }
+
+
+        if(view == buttonChoose){
+              showFileChooser();
+          }
 
     }
 }
